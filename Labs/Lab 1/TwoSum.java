0d59269f9c7646a995bda56fb85223ef82dddb83
Kvyheo <151234081+Kvyheo@users.noreply.github.com>
2024-02-07 22:24:57 -0800

    Update TwoSum.java

--- Labs/Lab 1/TwoSum.java
+++ Labs/Lab 1/TwoSum.java
@@ -4,17 +4,18 @@ public class TwoSum {
   public static int[] twoSum(int[] nums, int target) {
     HashMap<Integer, Integer> map = new HashMap<>();
 
-  for (int i = 0; i < nums.length(); i++) {
+  for (int i = 0; i < nums.length; i++) {
       int complement = target - nums[i];
     // Check if complement exists 
-    if (map.containsKey(a)) {
-        return new int[]{map.get(a),i};
+    if (map.containsKey(complement)) {
+        return new int[]{map.get(complement),i};
       }
     // if not add cur element and its index to map
     map.put(nums[i], i);
   }
     //No solution found
   throw new IllegalArgumentException("No two sum solution");
+  }
   
 
 public static void main(String[] args) {
@@ -22,4 +23,5 @@ public static void main(String[] args) {
   int target = 8;
   int[] result = twoSum(nums,target);
   System.out.println("Indices: " + result[0] + ", " + result[1]);
+}
 }
