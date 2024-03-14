8d4a0e8e5837a4bcd24709a8654f169258206bd3
Kvyheo <151234081+Kvyheo@users.noreply.github.com>
2024-03-13 18:54:12 -0700

    Create invertTree.py

---
+++ Labs/Lab4/invertTree.py
@@ -0,0 +1,8 @@
+class invertTree:
+  def invertTree(self, root):
+    if root is None:
+      return None
+
+      root.left, root.right = self.invertTree(root.right), self.invertTree(root.left)
+
+      return root
