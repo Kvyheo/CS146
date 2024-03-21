b0ec77e7c65ac6c0cb66b23c81fed9148d7390e6
Kvyheo <151234081+Kvyheo@users.noreply.github.com>
2024-03-20 23:16:59 -0700

    Create Solution.py

---
+++ HW/HW11/Solution.py
@@ -0,0 +1,14 @@
+class Solution:
+    def floodFill(self, image, sr, sc, newColor):
+        color = image[sr][sc]
+        if color != newColor:
+            self.dfs(image, sr, sc, color, newColor)
+        return image
+
+    def dfs(self, image, r, c, color, newColor):
+        if image[r][c] == color:
+            image[r][c] = newColor
+            if r >= 1: self.dfs(image, r-1, c, color, newColor)
+            if c >= 1: self.dfs(image, r, c-1, color, newColor)
+            if r+1 < len(image): self.dfs(image, r+1, c, color, newColor)
+            if c+1 < len(image[0]): self.dfs(image, r, c+1, color, newColor)
